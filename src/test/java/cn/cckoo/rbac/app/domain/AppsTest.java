bd192caec1f79ec51d1deaa5910ca11f14548350
Randall Cheng <randall.cheng@ringcentral.com>
2018-04-27 09:36:15 +0800

    modify some apps test

--- src/test/java/cn/cckoo/rbac/app/domain/AppsTest.java
+++ src/test/java/cn/cckoo/rbac/app/domain/AppsTest.java
@@ -7,7 +7,7 @@ import com.nitorcreations.junit.runners.NestedRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @RunWith(NestedRunner.class)
@@ -18,21 +18,24 @@ public class AppsTest {
     App app = AppBuilder.build();
 
     @Test
-    public void should_add_app() {
-        apps.add(app);
+    public void should_generate_token_and_add_app() {
+        apps.generateTokenAndAdd(app);
+
+        assertThat(app.getToken()).isNotNull();
+        assertThat(app.getToken().length()).isEqualTo(TOKEN_LENGTH);
         verify(mockAppRepo).save(app);
     }
 
     @Test
     public void should_return_true_when_save_with_no_exception() {
-        assertTrue(apps.add(app));
+        assertThat(apps.add(app)).isTrue();
     }
 
     @Test
     public void should_return_false_when_save_with_exception() {
         given_app_repo_save_will_failed();
 
-        assertTrue(!apps.add(app));
+        assertThat(!apps.add(app)).isTrue();
     }
 
     private void given_app_repo_save_will_failed() {
